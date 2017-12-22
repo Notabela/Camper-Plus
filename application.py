16daa8ce6dcb7fb3cbec374bec26d4aa5bc542c3
daniel <daniel.obeng@macaulay.cuny.edu>
2017-12-22 06:12:45 -0500

    updates requirements, pipfile, preps for deploy

--- application.py
+++ application.py
@@ -3,6 +3,4 @@
 from camperapp import app, db
 
 if __name__ == "__main__":
-    db.create_all()
-    db.session.commit()
     app.run(debug=True)
--- camperapp/__init__.py
+++ camperapp/__init__.py
@@ -8,9 +8,9 @@ from flask_login import LoginManager
 app = Flask(__name__)
 login_manager = LoginManager()
 
-db_path = "postgresql+psycopg2://localhost/camper_plus"
-mock_db_path = "sqlite:///test.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = db_path
+# db_path = "postgresql+psycopg2://localhost/camper_plus"
+# mock_db_path = "sqlite:///test.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = "0QZSIXW7U50133JZuZ6gpkwZ9yYCXY"
