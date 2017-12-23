534af737135ac5bf02f48455b099056cb6ed5f8c
daniel <daniel.obeng@macaulay.cuny.edu>
2017-12-23 16:09:45 -0500

    fixes parent page crashing

--- camperapp/__init__.py
+++ camperapp/__init__.py
@@ -8,8 +8,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 # db_path = "postgresql+psycopg2://localhost/camper_plus"
-# mock_db_path = "sqlite:///test.db"
-# app.config['SQLALCHEMY_DATABASE_URI'] = db_path
+mock_db_path = "sqlite:///test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = mock_db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "0QZSIXW7U50133JZuZ6gpkwZ9yYCXY"
 
